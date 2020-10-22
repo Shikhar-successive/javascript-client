ca1363a8ddf9d34207df286e0c995e9156e76e4a
Shikhar-successive <shikhar.singh@successive.tech>
2020-10-22 18:25:30 +0530

    feat: add permissions.js with logging

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -1,30 +1,32 @@
 const permissions = {
-               'getUsers': {
-               all: ['head-trainer','manager'],
-               read : ['trainee', 'trainer'],
-               write : ['trainer'],
-               delete: [],
-               },
-               
-             }
+     'getUsers': {
+     all: ['head-trainer','manager'],
+     read : ['trainee', 'trainer'],
+     write : ['trainer'],
+     delete: [],
+     },
+     
+   }
 
 function hasPermissions(moduleName,role,permissionType)
 {
-     if(Object.getOwnPropertyNames(permissions).includes(moduleName))                //Condition for checking correct "ModuleName"
-     {
-          if(permissions[moduleName].all.includes(role))                             //Condition for checking if "permissionType(all)" includes the specified "role"
-          {
-               console.log(permissions[moduleName].all.includes(role));              //prints true/false based on "role" present in "permissionType(all)"     
-          }
-          else
-          {
-          console.log(permissions[moduleName][permissionType].includes(role));        //returns true/false based on "role" present in "permissionType(other than all)"
-          }
-     }
-     else
-     {
-          console.log("Invalid Module Name");                                        //prints message if "ModuleName" is incorrect
-     }
+if(Object.getOwnPropertyNames(permissions).includes(moduleName))                //Condition for checking correct "ModuleName"
+{
+if(permissions[moduleName].all.includes(role))                             //Condition for checking if "permissionType(all)" includes the specified "role"
+{
+     console.log("Permission of",role,"in Module",moduleName,"of Type",permissionType,":"
+                 ,permissions[moduleName].all.includes(role));              //prints true/false based on "role" present in "permissionType(all)"     
+}
+else
+{
+console.log("Permission of",role,"in Module",moduleName,"of Type",permissionType,":"
+            ,permissions[moduleName][permissionType].includes(role));        //returns true/false based on "role" present in "permissionType(other than all)"
+}
+}
+else
+{
+console.log("Invalid Module Name");                                        //prints message if "ModuleName" is incorrect
+}
 }
 
 hasPermissions('getUsers','head-trainer','read');
