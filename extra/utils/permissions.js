6d6d188717020ff0383220c410ab1d1db7d8e839
Shikhar-successive <shikhar.singh@successive.tech>
2020-10-23 08:43:48 +0530

    Refactor: include return statement in function

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -15,7 +15,7 @@ function hasPermissions(moduleName,role,permissionType)
      if(permissions[moduleName].all.includes(role))                             //Condition for checking if "permissionType(all)" includes the specified "role"
      {
           console.log(">>Checking Permission of",role,"in Module",moduleName,"of Type",permissionType,":");
-          return (permissions[moduleName].all.includes(role));              //returns true/false based on "role" present in "permissionType(all)"     
+          return permissions[moduleName].all.includes(role);              //returns true/false based on "role" present in "permissionType(all)"     
      }
      else
      {
@@ -26,7 +26,7 @@ function hasPermissions(moduleName,role,permissionType)
      else
      {
           console.log("\n>>Checking Permission of",role,"in Module",moduleName,"of Type",permissionType,":");
-          return ("Invalid Module Name");                                        //prints message if "ModuleName" is incorrect
+          return Object.getOwnPropertyNames(permissions).includes(moduleName)                                        //prints message if "ModuleName" is incorrect
      }
      }
      
